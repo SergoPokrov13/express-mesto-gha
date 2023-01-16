56f1e8c6c0e4d2d1807d11cfbf48d8facce7e4f6
Sergey Pokrovsky <sapokrovsky@gmail.com>
2023-01-16 19:06:44 +0300

    autotest PLS

--- middlewares/errorsHandler.js
+++ middlewares/errorsHandler.js
@@ -1,11 +1,12 @@
+const { SERVER_ERROR_CODE } = require('../utils/statusError');
+
 function errorsHandler(err, req, res, next) {
-  const { statusCode = 500, message } = err;
+  const { statusCode = SERVER_ERROR_CODE, message } = err;
   res.status(statusCode).send({
-    message: statusCode === 500
+    message: statusCode === SERVER_ERROR_CODE
       ? 'На сервере произошла ошибка'
       : message,
   });
   return next();
 }
-
 module.exports = errorsHandler;
